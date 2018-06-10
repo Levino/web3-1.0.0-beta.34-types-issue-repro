4a8842777dc9428ec57207bb9d6d93aa0ea09731
Levin Keller <post@levinkeller.de>
2018-06-10 20:08:33 +0200

    Fix non working web3 require

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import Web3 = require('web3');
+import * as React from "react";
+import Web3 = require("web3");
 
-import './App.scss';
+import "./App.scss";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 const web3 = new Web3("ws://localhost:8546");
 
@@ -16,7 +16,7 @@ class App extends React.Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          You are using web3 at version {web3.version}
         </p>
       </div>
     );
