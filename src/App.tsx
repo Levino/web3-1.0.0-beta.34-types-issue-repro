9ed05e0dccbe69d7a07c1fae3a16a9d7babed600
Arseniy Ivanov <freeatnet@freeatnet.com>
2018-06-10 18:12:49 +0300

    Try importing web3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
+import Web3 = require('web3');
+
 import './App.scss';
 
 import logo from './logo.svg';
+
+const web3 = new Web3("ws://localhost:8546");
 
 class App extends React.Component {
   public render() {
